de82c31c14fffecd8c3090f7112d109df832a1aa
git <samimadane23@gmail.com>
2025-07-15 09:10:40 +0300

    feat: add app icon
    feat: implement splash screen
    
    feat: scaffold onboarding structure

---
+++ lib/features/onboarding/data/datasources/onboarding_data.dart
@@ -0,0 +1,20 @@
+// import 'package:easy_localization/easy_localization.dart';
+
+
+//  List<OnboardingModel> onboardingPages = [
+//   OnboardingModel(
+//     title: 'Welcome to LeukoAI'.tr(),
+//     subtitle: 'An intelligent assistant for early leukemia detection & smart patient care.'.tr(),
+//     imagePath: AssetsManager.onboarding1Image,
+//   ),
+//   OnboardingModel(
+//     title: 'AI-Powered Diagnosis'.tr(),
+//     subtitle: 'Upload blood samples and let AI help in fast, accurate analysis.'.tr(),
+//     imagePath: AssetsManager.onboarding2Image,
+//   ),
+//   OnboardingModel(
+//     title: 'Tailored for Everyone'.tr(),
+//     subtitle: 'Tailored experience for every role — manage, diagnose, and track with ease.'.tr(),
+//     imagePath: AssetsManager.onboarding3Image,
+//   ),
+// ];
---
+++ lib/features/onboarding/data/models/onboarding_model.dart
@@ -0,0 +1,11 @@
+class OnboardingModel {
+  final String title;
+  final String subtitle;
+  final String imagePath;
+
+  const OnboardingModel({
+    required this.title,
+    required this.subtitle,
+    required this.imagePath,
+  });
+}
