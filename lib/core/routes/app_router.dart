b23ba6ba2b218b01f448e944e03cc027a02fb402
git <samimadane23@gmail.com>
2025-07-15 09:15:14 +0300

    - fake push for development

--- lib/core/routes/app_router.dart
+++ lib/core/routes/app_router.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:magicchat/core/routes/routes.dart';
 
 
 
@@ -9,14 +10,14 @@ class AppRouter {
     //this arguments to be passed in any screen like this ( arguments as ClassName )
     final arguments = settings.arguments;
 
-    switch (settings.name) {
-      // case Routes.onBoardingScreen:
-      //   return MaterialPageRoute(
-      //     builder: (_) => const onBoardingScreen(),
-      //   );
+    // switch (settings.name) {
+    //   case Routes.onBoardingScreen:
+    //     return MaterialPageRoute(
+    //       // builder: (_) => const OnboardingScreen(),
+    //     );
      
-      default:
-        return null;
-    }
+      // default:
+    //   //   return null;
+    // }
   }
 }
